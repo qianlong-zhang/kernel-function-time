3cfe9c89a2cc369a227ccdcf6e8e055dc294d864
qianlong <qianlong2006@gmail.com>
2018-10-23 17:23:07 +0800

    add LTTng

--- reset_ftrace.sh
+++ reset_ftrace.sh
@@ -6,6 +6,8 @@ SYS_FTRACE_PATH=/sys/kernel/debug/tracing
 
 cd $SYS_FTRACE_PATH
 trace-cmd reset
+#echo ffffff,ffffffff,ffffffff,ffffffff,ffffffff,ffffffff,ffffffff,ffffffff,ffffffff,ffffffff > tracing_cpumask 
+echo ff > tracing_cpumask 
 echo 0 > trace
 echo 0 > tracing_on
 echo 1 > /proc/sys/kernel/ftrace_enabled
